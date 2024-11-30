1311853bb4e07e7fd8278fa3655282793a653606
Nate <1505703611@qq.com>
2024-11-30 21:24:41 +0000

    fix: change the file paths

--- src/detection.py
+++ src/detection.py
@@ -10,12 +10,14 @@ print(f'Using device: {device}')
 detection_model = pw_detection.MegaDetectorV6(device=device, pretrained=True, version="yolov9c")
 dataset_path = r"F:\DATASET\NACTI\images\nacti_part0"
 
-results = detection_model.batch_image_detection(dataset_path, batch_size=32)
-batch_output = r'G:\result\annotated_images'
-crop_output = r'G:\result\crops'
-pw_utils.save_detection_images(results, "batch_output", overwrite=False)
-pw_utils.save_crop_images(results, "crop_output", overwrite=False)
+results = detection_model.batch_image_detection(dataset_path, batch_size=128)
+batch_output = r'E:\result\annotated_images'
+crop_output = r'E:\result\crops'
+print("Saving results...")
 pw_utils.save_detection_json(results, os.path.join(".","part0output.json"),
                              categories=detection_model.CLASS_NAMES,
                              exclude_category_ids=[], # Category IDs can be found in the definition of each model.
                              exclude_file_path=None)
+# Save images
+pw_utils.save_detection_images(results, "batch_output", overwrite=False)
+pw_utils.save_crop_images(results, "crop_output", overwrite=False)
