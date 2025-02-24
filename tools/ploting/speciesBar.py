1c2aae29b0288e108ebc967ac49cace60e5ecb28
Nate <1505703611@qq.com>
2025-02-24 21:59:14 +0000

    feat: using long tailed plot strategy

--- tools/ploting/speciesBar.py
+++ tools/ploting/speciesBar.py
@@ -5,16 +5,26 @@ datapath = r'/Users/zehualiu/Documents/GitHub/Project-Prep/metadata/nacti_metada
 df = pd.read_csv(datapath)
 
 species_counts = df['common_name'].value_counts()
-print(species_counts)
 
-plt.figure(figsize=(10, 20))
-ax = species_counts.plot(kind='barh')
-plt.xlabel('Count')
-plt.ylabel('Species')
-plt.title('Species Count in Dataset')
+plt.figure(figsize=(50, 25))
+
+cmap = plt.get_cmap("coolwarm")
+colors = cmap(species_counts.rank(pct=True).values)
+
+bars = plt.bar(species_counts.index, species_counts.values, color=colors)
+
+plt.yscale("log")
+
+for bar, value in zip(bars, species_counts.values):
+    plt.text(bar.get_x() + bar.get_width()/2, value, f"{value}",
+             ha='center', va='bottom', fontsize=16, color="black", rotation=45)
+
+plt.ylabel('Count (log scale)', fontsize=16)
+plt.xlabel('Species', fontsize=16)
+# plt.title(f'Species Count in NACTI Dataset', fontsize=14)
+plt.xticks(rotation=90, ha='right',fontsize=16)
+plt.yticks(fontsize=16)
+plt.grid(axis='y', linestyle='--', alpha=0.6)
 plt.tight_layout()
-
-for index, value in enumerate(species_counts):
-    ax.text(value, index, str(value), va='center', fontsize=8, color='black')
-
+plt.savefig('species_count_vertical.pdf', dpi=1200)
 plt.show()
