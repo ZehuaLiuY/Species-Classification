9b7c792203123e023a06f9f771dcbce43ec0fba7
Nate <1505703611@qq.com>
2025-03-28 13:18:50 +0000

    feat: plot unbiased dataset

--- tools/ploting/pie.py
+++ tools/ploting/pie.py
@@ -1,46 +1,28 @@
+import json
 import matplotlib.pyplot as plt
-import pandas as pd
 
-data = {
-    "Class": [
-        "american black bear", "american marten", "american red squirrel",
-        "black-tailed jackrabbit", "bobcat", "california ground squirrel",
-        "california quail", "cougar", "coyote", "dark-eyed junco",
-        "domestic cow", "domestic dog", "donkey", "dusky grouse",
-        "eastern gray squirrel", "elk", "ermine", "european badger",
-        "gray fox", "gray jay", "horse", "house wren", "long-tailed weasel",
-        "moose", "mule deer", "nine-banded armadillo",
-        "north american porcupine", "north american river otter",
-        "raccoon", "red deer", "red fox", "snowshoe hare",
-        "steller's jay", "striped skunk", "unidentified accipitrid",
-        "unidentified bird", "unidentified chipmunk", "unidentified corvus",
-        "unidentified deer", "unidentified deer mouse", "unidentified mouse",
-        "unidentified pack rat", "unidentified pocket gopher",
-        "unidentified rabbit", "virginia opossum", "wild boar",
-        "wild turkey", "yellow-bellied marmot"
-    ],
-    "Prevalence": [
-        2962, 142, 330, 105, 2560, 3277, 365, 1639, 2215, 2, 15555, 187, 319, 0,
-        2918, 2420, 0, 16, 1034, 9, 28, 0, 5, 1166, 10706, 924, 48, 73, 3611,
-        15015, 170, 1389, 0, 1200, 30, 9156, 137, 117, 10793, 17, 0, 72, 0, 436,
-        136, 11519, 469, 42
-    ]
-}
+json_path = r"F:\DATASET\ENA24-Detection\metadata\ena24_updated.json"
 
-df = pd.DataFrame(data)
+with open(json_path, "r") as f:
+    data = json.load(f)
 
-df_nonzero = df[df["Prevalence"] > 0]
+annotation_counts = {}
+for ann in data["annotations"]:
+    cat_id = ann["category_id"]
+    annotation_counts[cat_id] = annotation_counts.get(cat_id, 0) + 1
 
-df_top = df_nonzero.sort_values("Prevalence", ascending=False).head(15)
-df_other = pd.DataFrame({
-    "Class": ["Other"],
-    "Prevalence": [df_nonzero["Prevalence"].sum() - df_top["Prevalence"].sum()]
-})
-df_pie = pd.concat([df_top, df_other], ignore_index=True)
+cat_names = {cat["id"]: cat["name"] for cat in data["categories"]}
 
-plt.figure(figsize=(10, 10))
-plt.pie(df_pie["Prevalence"], labels=df_pie["Class"], autopct='%1.1f%%', startangle=140)
-plt.title("Class Distribution in the Test Set (Top 15 Classes + Other)")
-plt.tight_layout()
-plt.savefig('class_distribution_pie.pdf', dpi=600)
+
+labels = []
+sizes = []
+for cat_id, count in annotation_counts.items():
+    labels.append(cat_names.get(cat_id, str(cat_id)))
+    sizes.append(count)
+
+plt.figure(figsize=(9,9))
+plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=140)
+plt.title("Unbiased Test Set Species Distribution")
+plt.axis("equal")
+plt.savefig("Unbiased Distribution.pdf", dpi=600)
 plt.show()
