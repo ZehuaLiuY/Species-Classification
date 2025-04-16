13d348110857c440ba46d486876b05a168bdc1c1
Nate <1505703611@qq.com>
2025-04-16 20:50:48 +0100

    feat: sorted by the prevelance

--- tools/ploting/predicted_frequency.py
+++ tools/ploting/predicted_frequency.py
@@ -5,15 +5,17 @@ json_path = r'G:\Code\github\Project-Prep\test_result\nonbiased\json\48\LDAM.jso
 with open(json_path, 'r') as f:
     data = json.load(f)
 
-filtered_items = [entry for entry in data if entry.get("ground_truth_class") in ["horse"]]
+filtered_items = [entry for entry in data if entry.get("ground_truth_class") == "wild turkey"]
+counter = Counter(entry["predicted_class"] for entry in filtered_items)
 
-counter = Counter(entry.get("predicted_class") for entry in filtered_items)
+sorted_items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+classes, freqs = zip(*sorted_items)
 
 plt.figure(figsize=(15, 6))
-plt.bar(list(counter.keys()), list(counter.values()))
+plt.bar(classes, freqs)
 plt.xlabel('Predicted Class')
 plt.ylabel('Frequency')
-plt.title('Predicted Frequency for Entries with Ground Truth "horse"')
+plt.title('Predicted Frequency for Entries with Ground Truth "wild turkey"')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.savefig('predicted_frequency_horse.pdf', dpi=600)
